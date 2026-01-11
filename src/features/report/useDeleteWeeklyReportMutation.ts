252cc129220694c8456294c571527459cec0765d
Alchernix <alchernix149@naver.com>
2026-01-11 23:06:44 +0900

    chore: 테스트용 버튼 추가

---
+++ src/features/report/useDeleteWeeklyReportMutation.ts
@@ -0,0 +1,30 @@
+// 주간 레포트 삭제(테스트용)
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { api } from "@/lib/axios";
+import useErrorStore from "@/store/errorStore";
+import type { AxiosError } from "axios";
+import type { ApiErrResponse } from "@/generated/api";
+
+export function useDeleteWeeklyReportMutation() {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: async () => {
+      const res = await api.post(`/api/v1/test/delete/weekly-report`);
+      return res.data;
+    },
+    onSuccess: () => {
+      queryClient.removeQueries({
+        queryKey: ["currentUser", "weeklyReport"],
+      });
+    },
+    onError: (err: AxiosError<ApiErrResponse<null>>) => {
+      useErrorStore.getState().showError(
+        // Todo: 에러 메시지 변경
+        err.response?.data?.code ?? err.message,
+        err.response?.data?.message ??
+          "알 수 없는 에러가 발생했습니다. 다시 시도해 주세요."
+      );
+    },
+  });
+}
