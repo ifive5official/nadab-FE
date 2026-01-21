af9a14a35740e065a96561b876202a8ea2740e0f
Alchernix <alchernix149@naver.com>
2026-01-21 19:04:53 +0900

    test: swiper 롤백

--- src/features/social/FriendsTab.tsx
+++ src/features/social/FriendsTab.tsx
@@ -1,14 +1,17 @@
 import Container from "@/components/Container";
-import { ChevronRightIcon } from "@/components/Icons";
+import { ChevronRightIcon, WarningFilledIcon } from "@/components/Icons";
 import InlineButton from "@/components/InlineButton";
 import SearchBar from "@/components/SearchBar";
 import { useState } from "react";
 import { NoResultIcon } from "@/components/Icons";
+import clsx from "clsx";
+import Modal from "@/components/Modal";
 
 export default function FriendsTab() {
   const [searchTerm, setSearchTerm] = useState("");
-  const friends = Array(0).fill(0); // 임시
-  const friendRequests = Array(0).fill(0); // 임시
+  const friends = Array(11).fill(0); // 임시
+  const friendRequests = Array(3).fill(0); // 임시
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   return (
     <>
@@ -16,14 +19,26 @@ export default function FriendsTab() {
         onChange={(e) => setSearchTerm(e.target.value)}
         value={searchTerm}
         onDeleteKeyword={() => setSearchTerm("")}
-        className="h-11 mx-padding-x-m"
+        className="h-11 mx-padding-x-m shrink-0"
       />
       {/* 친구 요청 미리보기 섹션 */}
       {friendRequests.length > 0 && (
         <div className="px-padding-x-m py-padding-y-m mt-margin-y-l flex items-center border-y border-y-interactive-border-default">
           <div className="flex mr-margin-x-l">
-            <div className="rounded-full aspect-square h-9 bg-neutral-300 -mt-4" />
-            <div className="rounded-full aspect-square h-9 bg-neutral-300 -ml-5 -mb-4" />
+            <div
+              className={clsx(
+                "rounded-full aspect-square h-9 bg-neutral-300",
+                friendRequests.length >= 2 && "-mt-4",
+              )}
+            />
+            {friendRequests.length === 2 && (
+              <div className="rounded-full aspect-square h-9 bg-neutral-300 -ml-5 -mb-4" />
+            )}
+            {friendRequests.length > 2 && (
+              <div className="rounded-full aspect-square h-9 -ml-5 -mb-4 flex items-center justify-center text-label-s bg-button-primary-bg-default border border-interactive-border-default dark:border-0 text-text-inverse-primary">
+                +{friendRequests.length - 1}
+              </div>
+            )}
           </div>
           <div className="flex flex-col mr-auto">
             <span className="text-label-m">친구 요청</span>
@@ -44,13 +59,17 @@ export default function FriendsTab() {
           친구 {friends.length}명
         </span>
         {friends.length > 0 ? (
-          <ul className="py-padding-y-m flex flex-col gap-gap-y-xl">
+          <ul className="pt-padding-y-m flex flex-col gap-gap-y-xl">
             {friends.map((_, i) => {
               return (
                 <li key={i} className="flex items-center gap-margin-x-l">
                   <div className="rounded-full aspect-square h-[50px] bg-neutral-300" />
                   <span className="text-button-1 mr-auto">알케르닉스</span>
-                  <InlineButton size="m" variant="secondary">
+                  <InlineButton
+                    size="m"
+                    variant="secondary"
+                    onClick={() => setIsModalOpen(true)}
+                  >
                     삭제
                   </InlineButton>
                 </li>
@@ -58,7 +77,14 @@ export default function FriendsTab() {
             })}
           </ul>
         ) : (
-          <div className="flex flex-col items-center gap-gap-y-l mt-[calc((110/796)*100dvh)] mb-auto">
+          <div
+            className={clsx(
+              "flex flex-col items-center gap-gap-y-l mb-auto",
+              friendRequests.length === 0
+                ? "mt-[calc((110/796)*100dvh)]"
+                : "mt-[calc((70/796)*100dvh)]",
+            )}
+          >
             <NoResultIcon className="p-3.5" />
             <p className="text-title-1">아직은 친구가 없어요.</p>
             <p className="text-caption-m">
@@ -67,6 +93,24 @@ export default function FriendsTab() {
           </div>
         )}
       </Container>
+      <Modal
+        title={`알케르닉스님을 친구에서 삭제하겠어요?`}
+        icon={WarningFilledIcon}
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        buttons={[
+          {
+            label: "취소",
+            onClick: () => setIsModalOpen(false),
+          },
+          {
+            label: "확인",
+            onClick: () => setIsModalOpen(false),
+          },
+        ]}
+      >
+        친구 삭제 이후에 복구가 불가능해요.
+      </Modal>
     </>
   );
 }
--- src/routes/_authenticated/_main/social.tsx
+++ src/routes/_authenticated/_main/social.tsx
@@ -1,15 +1,7 @@
 import SegmentedControls from "@/components/SegmentedControls";
 import FriendsTab from "@/features/social/FriendsTab";
 import { createFileRoute } from "@tanstack/react-router";
-import { useState, useRef } from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import type { Swiper as SwiperType } from "swiper/types";
-// @ts-ignore
-import "swiper/css";
-// @ts-ignore
-import "swiper/css/navigation";
-// @ts-ignore
-import "swiper/css/pagination";
+import { useState } from "react";
 import FeedTab from "@/features/social/FeedTab";
 import GroupTab from "@/features/social/GroupTab";
 
@@ -19,7 +11,6 @@ export const Route = createFileRoute("/_authenticated/_main/social")({
 
 function RouteComponent() {
   const [selected, setSelected] = useState("feed");
-  const swiperRef = useRef<SwiperType>(null);
 
   const tabs = [
     { label: "피드", value: "feed" },
@@ -27,44 +18,17 @@ function RouteComponent() {
     { label: "친구", value: "friend" },
   ];
 
-  function handleTabChange(value: string) {
-    setSelected(value);
-    const index = tabs.findIndex((tab) => tab.value === value);
-    if (swiperRef.current) {
-      swiperRef.current.slideTo(index);
-    }
-  }
-
-  function handleSlideChange(swiper: SwiperType) {
-    const currentIndex = swiper.activeIndex;
-    setSelected(tabs[currentIndex].value);
-  }
-
   return (
     <>
       <SegmentedControls
         options={tabs}
         selected={selected}
-        onChange={handleTabChange}
+        onChange={setSelected}
         className="my-padding-y-m mx-padding-x-m"
       />
-      <Swiper
-        onSwiper={(swiper) => (swiperRef.current = swiper)}
-        onSlideChange={handleSlideChange}
-        initialSlide={0}
-        autoHeight={true}
-        className="w-full flex-1"
-      >
-        <SwiperSlide>
-          <FeedTab />
-        </SwiperSlide>
-        <SwiperSlide>
-          <GroupTab />
-        </SwiperSlide>
-        <SwiperSlide>
-          <FriendsTab />
-        </SwiperSlide>
-      </Swiper>
+      {selected === "feed" && <FeedTab />}
+      {selected === "group" && <GroupTab />}
+      {selected === "friend" && <FriendsTab />}
     </>
   );
 }
