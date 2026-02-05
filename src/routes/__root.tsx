a4f0ee26ed4cb03bbb3f70b6bf2b8b4d927e5845
Alchernix <alchernix149@naver.com>
2026-02-05 21:37:26 +0900

    test: 스크롤 테스트

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -28,7 +28,7 @@ function RootComponent() {
 
   return (
     <>
-      <div className="min-h-dvh w-vw flex bg-surface-base">
+      <div className="min-h-full w-full flex bg-surface-base">
         <div className="flex flex-col w-dvw sm:w-[412px] sm:mx-auto overflow-x-hidden">
           <Outlet />
           <Modal />
