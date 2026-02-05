0cf3b1e09f79ffdd41b58890c868dd5d4e23e028
Alchernix <alchernix149@naver.com>
2026-02-06 03:10:59 +0900

    fix(ui): 감정 색깔 수정

--- src/constants/emotions.ts
+++ src/constants/emotions.ts
@@ -3,7 +3,7 @@ const emotions = [
   { title: "흥미", color: "#FFD29B", code: "INTEREST" },
   { title: "평온", color: "#B4E7E2", code: "PEACE" },
   { title: "즐거움", color: "#FFA7A9", code: "PLEASURE" },
-  { title: "의지", color: "#EEB78E", code: "WILL" },
+  { title: "의지", color: "#ABC0F2", code: "WILL" },
   { title: "우울", color: "#D3A7FF", code: "DEPRESSION" },
   { title: "후회", color: "#BDF2AB", code: "REGRET" },
   { title: "기타", color: "#D4D4D4", code: "ETC" },
