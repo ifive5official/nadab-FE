7f73da0b972814061631829b4941baaf5426ca10
Alchernix <alchernix149@naver.com>
2026-02-12 16:57:06 +0900

    feat(report): 유형 리포트 카테고리 제작

--- src/routes/_authenticated/_main/report.tsx
+++ src/routes/_authenticated/_main/report.tsx
@@ -6,6 +6,8 @@ import TotalReportTab from "@/features/report/TotalReportTab";
 import Container from "@/components/Container";
 import { periodicReportOptions } from "@/features/report/quries";
 import Loading from "@/components/Loading";
+import categories from "@/constants/categories";
+import clsx from "clsx";
 
 type Tab = "periodic" | "category";
 
@@ -36,6 +38,8 @@ export const Route = createFileRoute("/_authenticated/_main/report")({
 
 function RouteComponent() {
   const [selected, setSelected] = useState("periodic");
+  const [selectedCategory, setSelectedCategory] =
+    useState<(typeof categories)[number]["code"]>("PREFERENCE");
 
   return (
     <Container hasHeader={false}>
@@ -48,6 +52,27 @@ function RouteComponent() {
         className="my-padding-y-m"
         onChange={setSelected}
       />
+      {selected === "category" && (
+        <ul className="shrink-0 flex items-center gap-gap-x-s mb-margin-y-l overflow-x-scroll [scrollbar-width:none] [&::-webkit-scrollbar]:hidden">
+          {categories.map((category) => {
+            const selected = category.code === selectedCategory;
+            return (
+              <li
+                onClick={() => setSelectedCategory(category.code)}
+                key={category.code}
+                className={clsx(
+                  "rounded-lg px-padding-x-m py-1.5 text-button-2 whitespace-pre border",
+                  selected
+                    ? "bg-brand-primary border-brand-primary text-button-primary-text-default"
+                    : "bg-surface-layer-1 border-button-tertiary-border-default text-interactive-text-hover",
+                )}
+              >
+                {category.title}
+              </li>
+            );
+          })}
+        </ul>
+      )}
       <div className="relative -mx-padding-x-m">
         <div className="border-b border-b-interactive-border-default w-full" />
       </div>
