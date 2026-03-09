d39cc10405f557176610714d75dcef2b593524b3
Alchernix <alchernix149@naver.com>
2026-03-10 00:05:58 +0900

    test: 개발 환경에서 테스트 완료

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,30 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import viteReact from "@vitejs/plugin-react";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import { fileURLToPath, URL } from "node:url";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
-  return {
-    plugins: [
-      tanstackRouter({
-        target: "react",
-        autoCodeSplitting: true,
-      }),
-      viteReact(),
-      tailwindcss(),
-    ],
-    resolve: {
-      alias: {
-        "@": fileURLToPath(new URL("./src", import.meta.url)),
+export default defineConfig({
+  plugins: [
+    tanstackRouter({
+      target: "react",
+      autoCodeSplitting: true,
+    }),
+    viteReact(),
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://nadab-dev.n-e.kr",
+        changeOrigin: true,
       },
     },
-    server: {
-      proxy: {
-        "/api": {
-          target: env.VITE_API_URL,
-          changeOrigin: true,
-        },
-      },
-    },
-  };
+  },
 });
