0a0612edb349d7e35fa93a3c8bb915c5c3d1f8e0
Alchernix <alchernix149@naver.com>
2026-04-15 23:41:30 +0900

    feat(auth): 애플 로그인 추가

--- src/features/LandingPage.tsx
+++ src/features/LandingPage.tsx
@@ -16,6 +16,10 @@ import { useEffect } from "react";
 import { Browser } from "@capacitor/browser";
 import { Capacitor3KakaoLogin } from "capacitor3-kakao-login";
 import clsx from "clsx";
+import {
+  SignInWithApple,
+  type SignInWithAppleResponse,
+} from "@capacitor-community/apple-sign-in";
 
 type UrlRes = components["schemas"]["AuthorizationUrlResponse"];
 
@@ -76,13 +80,53 @@ export function LandingPage() {
     }
   }, []);
 
-  async function sdkGoogleLogin() {
+  async function handleNativeLogin(
+    provider: "google" | "naver" | "kakao" | "apple",
+  ) {
     try {
-      const user = await GoogleAuth.signIn();
-      const res = await api.post("/api/v1/auth/google/native-login", {
-        googleIdToken: user.authentication.idToken,
-      });
-      const { accessToken, signupStatus } = res.data.data!;
+      let res;
+      switch (provider) {
+        case "google":
+          {
+            const user = await GoogleAuth.signIn();
+            res = await api.post("/api/v1/auth/google/native-login", {
+              googleIdToken: user.authentication.idToken,
+            });
+          }
+          break;
+        case "naver":
+          {
+            const user = await CapacitorNaverLogin.login();
+            res = await api.post("/api/v1/auth/naver/native-login", {
+              naverAccessToken: user.accessToken,
+            });
+          }
+          break;
+        case "kakao":
+          {
+            const user = await Capacitor3KakaoLogin.kakaoLogin();
+            res = await api.post("/api/v1/auth/kakao/native-login", {
+              kakaoAccessToken: user.value,
+            });
+          }
+          break;
+        case "apple":
+          {
+            const result: SignInWithAppleResponse =
+              await SignInWithApple.authorize({
+                clientId: "com.nadab.app",
+                redirectURI: "/",
+                scopes: "email name",
+              });
+
+            res = await api.post("/api/v1/auth/apple/native-login", {
+              code: result.response.authorizationCode,
+            });
+          }
+          break;
+      }
+
+      const { accessToken, signupStatus } = res!.data.data!;
 
       useAuthStore.getState().setAccessToken(accessToken!);
 
@@ -96,72 +140,9 @@ export function LandingPage() {
     } catch (err) {
       if (
         axios.isAxiosError(err) &&
-        err.response?.data?.code ===
-          "AUTH_EMAIL_ALREADY_REGISTERED_WITH_DIFFERENT_METHOD"
-      ) {
-        // 이미 일반 로그인으로 가입한 계정일 시
-        useModalStore
-          .getState()
-          .showError("이미 가입한 계정이에요.", "다른 계정으로 가입해보세요.");
-      } else if (axios.isAxiosError(err)) {
-        handleDefaultApiError(err);
-      }
-    }
-  }
-
-  async function sdkNaverLogin() {
-    try {
-      const user = await CapacitorNaverLogin.login();
-      const res = await api.post("/api/v1/auth/naver/native-login", {
-        naverAccessToken: user.accessToken,
-      });
-      const { accessToken, signupStatus } = res.data.data!;
-
-      useAuthStore.getState().setAccessToken(accessToken!);
-
-      if (signupStatus === "PROFILE_INCOMPLETE") {
-        navigate({
-          to: "/signup/terms",
-          replace: true,
-          search: { type: "social" },
-        });
-      }
-    } catch (err) {
-      if (
-        axios.isAxiosError(err) &&
-        err.response?.data?.code === "AUTH_EMAIL_ALREADY_REGISTERED_WITH_BASIC"
-      ) {
-        // 이미 일반 로그인으로 가입한 계정일 시
-        useModalStore
-          .getState()
-          .showError("이미 가입한 계정이에요.", "다른 계정으로 가입해보세요.");
-      } else if (axios.isAxiosError(err)) {
-        handleDefaultApiError(err);
-      }
-    }
-  }
-
-  async function sdkKakaoLogin() {
-    try {
-      const user = await Capacitor3KakaoLogin.kakaoLogin();
-      const res = await api.post("/api/v1/auth/kakao/native-login", {
-        kakaoAccessToken: user.value,
-      });
-      const { accessToken, signupStatus } = res.data.data!;
-
-      useAuthStore.getState().setAccessToken(accessToken!);
-
-      if (signupStatus === "PROFILE_INCOMPLETE") {
-        navigate({
-          to: "/signup/terms",
-          replace: true,
-          search: { type: "social" },
-        });
-      }
-    } catch (err) {
-      if (
-        axios.isAxiosError(err) &&
-        err.response?.data?.code === "AUTH_EMAIL_ALREADY_REGISTERED_WITH_BASIC"
+        err.response?.data?.code.startsWith(
+          "AUTH_EMAIL_ALREADY_REGISTERED_WITH_",
+        )
       ) {
         // 이미 일반 로그인으로 가입한 계정일 시
         useModalStore
@@ -195,21 +176,21 @@ export function LandingPage() {
       <div className="flex-1 w-full flex flex-col justify-center py-padding-y-m">
         <div className="flex flex-col gap-gap-y-m">
           <SocialLoginButton
-            onNativeClick={sdkNaverLogin}
+            onNativeClick={() => handleNativeLogin("naver")}
             url={socialLoginUrls?.naver}
             icon={NaverIcon}
             title="네이버로 로그인"
             className="bg-[#03C75A] text-white"
           />
           <SocialLoginButton
-            onNativeClick={sdkKakaoLogin}
+            onNativeClick={() => handleNativeLogin("kakao")}
             url={socialLoginUrls?.kakao}
             icon={KakaoIcon}
             title="카카오로 로그인"
             className="bg-[#FEE500] text-[#0D0000]"
           />
           <SocialLoginButton
-            onNativeClick={sdkGoogleLogin}
+            onNativeClick={() => handleNativeLogin("google")}
             url={socialLoginUrls?.google}
             icon={() => (
               <img className="w-3.5 aspect-square" src="icon/googleIcon.png" />
@@ -217,12 +198,14 @@ export function LandingPage() {
             title="구글로 로그인"
             className="bg-[#F2F2F2] text-[#1F1F1F]"
           />
-          <SocialLoginButton
-            onNativeClick={() => {}}
-            icon={AppleIcon}
-            title="Apple로 로그인"
-            className="bg-black dark:bg-white text-white border dark:text-black border-[#4D4D4D]"
-          />
+          {Capacitor.getPlatform() === "ios" && (
+            <SocialLoginButton
+              onNativeClick={() => handleNativeLogin("apple")}
+              icon={AppleIcon}
+              title="Apple로 로그인"
+              className="bg-black dark:bg-white text-white border dark:text-black border-[#4D4D4D]"
+            />
+          )}
         </div>
         <div className="pt-padding-y-l pb-padding-y-xs flex flex-col gap-gap-y-l">
           <div className="flex items-center gap-5">
