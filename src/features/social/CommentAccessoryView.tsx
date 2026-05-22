5dda9f11b316eaafe5802d4311fd315ab92b4a5c
Alchernix <alchernix149@naver.com>
2026-05-23 01:02:50 +0900

    feat(comment): 댓글 및 비밀 댓글 기능 구현

--- src/features/social/CommentAccessoryView.tsx
+++ src/features/social/CommentAccessoryView.tsx
@@ -1,25 +1,31 @@
 // 키보드 위에 올라오는 악세서리 뷰
 // 댓글 작성 시 사용
 import { motion } from "motion/react";
-import { useKeyboardOffset } from "@/hooks/useKeyboardOffset";
+// import { useKeyboardOffset } from "@/hooks/useKeyboardOffset";
 import CommentInput from "./CommentInput";
 import CheckBox from "@/components/Checkbox";
 import { useState } from "react";
+import { usePostCommentMutation } from "./commentQueries";
 
-export default function CommentAccessoryView() {
-  const { bottomOffset } = useKeyboardOffset();
+type Props = {
+  dailyReportId: number;
+};
+
+export default function CommentAccessoryView({ dailyReportId }: Props) {
+  // const { bottomOffset } = useKeyboardOffset();
   const [isSecret, setIsSecret] = useState(false);
   const [content, setContent] = useState("");
+  const postCommentMutation = usePostCommentMutation();
 
   return (
     <motion.div
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
-      style={{
-        paddingBottom: `${bottomOffset}px`,
-      }}
-      className="w-full sm:w-[412px] sm:mx-auto fixed bottom-0 inset-x-0 h-16 flex items-center gap-padding-x-s px-padding-x-s border-t border-t-border-base"
+      // style={{
+      //   paddingBottom: `${bottomOffset}px`,
+      // }}
+      className="bg-surface-base w-full sm:w-[412px] sm:mx-auto fixed bottom-(--safe-bottom) inset-x-0 h-16 flex items-center gap-padding-x-s px-padding-x-s border-t border-t-border-base"
     >
       <div
         // 엑세서리 바 누를 때 포커스 이탈 방지
@@ -37,11 +43,19 @@ export default function CommentAccessoryView() {
           className="whitespace-pre"
         />
       </div>
-
-      <CommentInput
-        value={content}
-        onChange={(e) => setContent(e.target.value)}
-      />
+      <form
+        className="w-full"
+        onSubmit={(e) => {
+          e.preventDefault();
+          setContent("");
+          postCommentMutation.mutate({ dailyReportId, content, isSecret });
+        }}
+      >
+        <CommentInput
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+        />
+      </form>
     </motion.div>
   );
 }
