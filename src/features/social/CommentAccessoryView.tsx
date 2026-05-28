35c83e65dd9566561c9b7ba8eee1689ee13b5d5d
Alchernix <alchernix149@naver.com>
2026-05-28 17:59:27 +0900

    fix: 버그 수정

--- src/features/social/CommentAccessoryView.tsx
+++ src/features/social/CommentAccessoryView.tsx
@@ -14,6 +14,7 @@ import useToastStore from "@/store/toastStore";
 import useCommentInputStore from "@/store/commentInputStore";
 import useModalStore from "@/store/modalStore";
 import { CircleCheckFilledIcon } from "@/components/Icons";
+import { Capacitor } from "@capacitor/core";
 
 export default function CommentAccessoryView() {
   const {
@@ -86,13 +87,16 @@ export default function CommentAccessoryView() {
 
   // 부모가 비밀댓글이면 자식은 무조건 비밀댓글
   const finalIsSecret = isParentSecret ? true : isSecret;
+  const platform = Capacitor.getPlatform();
+
   return (
     <motion.div
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
       className={clsx(
-        "bg-surface-base dark:bg-surface-layer-2 w-full sm:w-[412px] sm:mx-auto fixed bottom-(--safe-bottom) inset-x-0 flex items-center gap-padding-x-s px-padding-x-s border-t border-t-border-base dark:border-t-border-layer-1",
+        "bg-surface-base dark:bg-surface-layer-2 w-full sm:w-[412px] sm:mx-auto fixed inset-x-0 flex items-center gap-padding-x-s px-padding-x-s border-t border-t-border-base dark:border-t-border-layer-1",
+        platform === "ios" ? "bottom-0" : "bottom-(--safe-bottom)",
         parentCommentAuthorNickname ? "h-[104px]" : "h-16",
       )}
     >
